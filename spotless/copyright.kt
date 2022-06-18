10984fa305287712b5dd94d2d3af2cccda1e331a
PatilShreyas <shreyaspatilg@gmail.com>
2022-06-18 22:24:11 +0530

    Setup spotless and reformat with it

--- spotless/copyright.kt
+++ spotless/copyright.kt
@@ -13,19 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.shreyaspatil.permissionFlow
-
-import org.junit.Assert.assertEquals
-import org.junit.Test
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-}
