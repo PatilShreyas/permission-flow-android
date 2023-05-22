0fd90307b92a7a9169e951460f74f4fd8da842b9
Shreyas Patil <shreyaspatilg@gmail.com>
2023-05-22 19:00:50 +0530

    #54 | Reformat with spotless

--- app/src/main/java/dev/shreyaspatil/permissionFlow/example/ui/fragment/ExampleFragment.kt
+++ app/src/main/java/dev/shreyaspatil/permissionFlow/example/ui/fragment/ExampleFragment.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2022 Shreyas Patil
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.shreyaspatil.permissionFlow.example.ui.fragment
 
 import android.os.Bundle
@@ -31,7 +46,7 @@ class ExampleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         _binding = ViewFragmentExampleBinding.inflate(inflater, null, false)
         return binding.root
@@ -54,11 +69,11 @@ class ExampleFragment : Fragment() {
             permissionFlow.getMultiplePermissionState(*permissions).collect {
                 val grantedPermissionsText = it.grantedPermissions.joinToString(
                     separator = "\n",
-                    prefix = "Granted Permissions:\n"
+                    prefix = "Granted Permissions:\n",
                 )
                 val deniedPermissionsText = it.deniedPermissions.joinToString(
                     separator = "\n",
-                    prefix = "Denied Permissions:\n"
+                    prefix = "Denied Permissions:\n",
                 )
 
                 binding.grantedPermissionsText.text = grantedPermissionsText
